17605bf76c4eda4398c8641029ea33426ec04a46
Antoine G <grugeon.antoine@gmail.com>
2022-04-26 14:09:20 +0200

    Tag added and umbrella display started

--- Yochi 2 Return of the Nyah/Assets/Scripts/YochiUmbrella.cs
+++ Yochi 2 Return of the Nyah/Assets/Scripts/YochiUmbrella.cs
@@ -5,21 +5,56 @@ using BulletPro;
 
 public class YochiUmbrella : MonoBehaviour
 {
+    public EmitterProfile yokaiEmitter;
+    public EmitterProfile realEmitter;
+
     public BulletEmitter bulletEmitter;
     public GameObject umbrella;
     public float aimCursorDistance;
+    public Sprite[] realUmbrellaOrientations;
+    public Sprite[] yokaiUmbrellaOrientations;
+    public SpriteRenderer umbrellaRenderer;
 
     private Vector2 aimInput;
     private Vector2 aimDirection;
-
     private bool isShooting;
+    private float umbrellaAngle;
 
     void Start()
     {
-        
+        SwitchEmitter(false);
     }
 
     void Update()
+    {
+        UpdateInput();
+        UpdateUmbrellaOrientation();
+    }
+
+    void Shoot()
+    {
+        if(!isShooting)
+        {
+            isShooting = true;
+            bulletEmitter.Play();
+        }
+    }
+
+    public void SwitchEmitter(bool isYokai)
+    {
+        bulletEmitter.Stop();
+        isShooting = false;
+        if (isYokai)
+        {
+            bulletEmitter.SwitchProfile(yokaiEmitter, false, PlayOptions.DoNothing);
+        }
+        else
+        {
+            bulletEmitter.SwitchProfile(realEmitter, false, PlayOptions.DoNothing);
+        }
+    }
+
+    private void UpdateInput()
     {
         aimInput = new Vector2(Input.GetAxis("RightStickH"), Input.GetAxis("RightStickV"));
 
@@ -27,12 +62,13 @@ public class YochiUmbrella : MonoBehaviour
         if (aimInput.magnitude > 0.3f)
         {
             aimDirection = aimInput.normalized;
+            umbrellaAngle = Vector2.SignedAngle(Vector2.right, aimDirection);
 
             umbrella.transform.localPosition = aimCursorDistance * aimDirection;
-            bulletEmitter.transform.rotation = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.up, aimDirection));
+            bulletEmitter.transform.rotation = Quaternion.Euler(0, 0, umbrellaAngle - 90);
         }
 
-        if(Input.GetAxis("RightTrigger") == 1)
+        if (Input.GetAxis("RightTrigger") == 1)
         {
             Shoot();
         }
@@ -44,12 +80,21 @@ public class YochiUmbrella : MonoBehaviour
         }
     }
 
-    void Shoot()
+    private void UpdateUmbrellaOrientation()
     {
-        if(!isShooting)
+        int indexOrientation = 0;
+        while(umbrellaAngle > (22.5f + (indexOrientation * 45f)))
         {
-            isShooting = true;
-            bulletEmitter.Play();
+            indexOrientation++;
+        }
+
+        if(YochiManager.instance.isInYokaiWorld)
+        {
+            umbrellaRenderer.sprite = yokaiUmbrellaOrientations[indexOrientation];
+        }
+        else
+        {
+            umbrellaRenderer.sprite = realUmbrellaOrientations[indexOrientation];
         }
     }
 }
