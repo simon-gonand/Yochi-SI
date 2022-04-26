afca7f58667e16c9015b410496041b5556d5c338
Arthur <arthurgpro@outlook.fr>
2022-04-26 12:55:55 +0200

    Plateforme changement de monde v1  et test tiles

--- Yochi 2 Return of the Nyah/Assets/Scripts/PlateformeMonoMonde.cs
+++ Yochi 2 Return of the Nyah/Assets/Scripts/PlateformeMonoMonde.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class PlateformeMonoMonde : MonoBehaviour
 {
+    public Sprite yokaiWorldVisibleSprite;
+    public Sprite realWorldVisibleSprite;
+    public Sprite yokaiWorldInvisibleSprite;
+    public Sprite realWorldInvisibleSprite;
+    public bool isForYokaiWorld;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D collider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
+        ChangeSprite();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeSprite();
+    }
+
+    void ChangeSprite()
+    {
+        if (isForYokaiWorld)
+        {
+            if (YochiManager.instance.isInYokaiWorld)
+            {
+                spriteRenderer.sprite = yokaiWorldVisibleSprite;
+                collider.enabled = true;
+            }
+            else
+            {
+                spriteRenderer.sprite = yokaiWorldInvisibleSprite;
+                collider.enabled = false;
+            }
+        }
+        else
+        {
+            if (YochiManager.instance.isInYokaiWorld)
+            {
+                spriteRenderer.sprite = realWorldInvisibleSprite;
+                collider.enabled = false;
+            }
+            else
+            {
+                spriteRenderer.sprite = realWorldVisibleSprite;
+                collider.enabled = true;
+            }
+        }
     }
 }
