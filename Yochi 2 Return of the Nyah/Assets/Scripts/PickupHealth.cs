c7473e50cd374a5e7fd29849dfaf10babf85d678
Simon <simongonand@gmail.com>
2022-04-29 19:37:43 +0200

    clamp max hp

--- Yochi 2 Return of the Nyah/Assets/Scripts/PickupHealth.cs
+++ Yochi 2 Return of the Nyah/Assets/Scripts/PickupHealth.cs
@@ -24,6 +24,7 @@ public class PickupHealth : MonoBehaviour
     {
         if (col.tag == "Player")
         {
+            if (YochiManager.instance.currentHealthPoint == YochiManager.instance.maxHealthPoint) return;
             col.GetComponent<YochiManager>().getHP(numberOfHPHeal);
             Destroy(gameObject);
         }
