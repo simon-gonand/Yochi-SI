70f6ea530ff8865ecd73c5fabf8c5b3fcfd262d2
Simon <simongonand@gmail.com>
2022-04-29 16:56:51 +0200

    fix main menu to be use with the controller

--- Yochi 2 Return of the Nyah/Assets/Scripts/MenuManager.cs
+++ Yochi 2 Return of the Nyah/Assets/Scripts/MenuManager.cs
@@ -1,22 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject mainMenuUI;
     public GameObject creditsScreen;
+    public Button startButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        startButton.Select();
     }
 
     public void StartGame()
@@ -41,6 +38,5 @@ public class MenuManager : MonoBehaviour
     public void LeaveGame()
     {
         Application.Quit();
-        Debug.Log("Leave");
     }
 }
